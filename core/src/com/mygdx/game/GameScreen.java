ff6d9b771a9cd6b509e6784c30fa1c7057a2a60a
JofreBa <a21jofbalgon@inspedralbes.cat>
2023-05-02 13:11:53 +0200

    edifici

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -36,7 +36,7 @@ public class GameScreen implements Screen {
 	public PerspectiveCamera cam;
 	static Array<ModelInstance> instances;
 	Model firstFloor,interiorfirstFloor,middleFloor,interiorMiddleFloor,topFloor,interiorTopFloor;
-	
+	final float SCALE = 0.125f;
 	public Environment environment;
 	
 	CamControl camController;
@@ -107,30 +107,68 @@ public class GameScreen implements Screen {
 		assets.load("floors/FirstFloor.g3db",Model.class);
 		assets.load("floors/InteriorFirstFloor.g3db",Model.class);
 		assets.load("floors/MiddleFloor.g3db",Model.class);
+		assets.load("floors/InteriorMiddleFloor.g3db",Model.class);
+		assets.load("floors/InteriorTopFloor.g3db",Model.class);
 		assets.load("floors/TopFloor.g3db",Model.class);
 		assets.finishLoading();
 
 		firstFloor = assets.<Model>get("floors/FirstFloor.g3db");
 		interiorfirstFloor = assets.<Model>get("floors/InteriorFirstFloor.g3db");
 		middleFloor = assets.<Model>get("floors/MiddleFloor.g3db");
+		interiorMiddleFloor = assets.<Model>get("floors/InteriorMiddleFloor.g3db");
 		topFloor = assets.<Model>get("floors/TopFloor.g3db");
+		interiorTopFloor = assets.<Model>get("floors/InteriorTopFloor.g3db");
 
-		ModelInstance firstFloorIn = new ModelInstance(firstFloor);
+		/*ModelInstance firstFloorIn = new ModelInstance(firstFloor);
 		ModelInstance interiorFirstFloorIn = new ModelInstance(interiorfirstFloor);
 		ModelInstance middleFloorIn = new ModelInstance(middleFloor);
 		ModelInstance topFloorIn = new ModelInstance(topFloor);
 
-		System.out.println(middleFloor.meshes.first().calculateBoundingBox().getHeight());
+
 		interiorFirstFloorIn.transform = new Matrix4(new Vector3(0,0,0),new Quaternion(),new Vector3(0.125f,0.125f,0.125f));
 		firstFloorIn.transform = new Matrix4(new Vector3(0,0,0),new Quaternion(),new Vector3(0.125f,0.125f,0.125f));
-		middleFloorIn.transform = new Matrix4(new Vector3(0,middleFloor.meshes.first().calculateBoundingBox().getHeight()/2+middleFloor.meshes.first().calculateBoundingBox().getHeight()/0.125f*1.5f,middleFloor.meshes.first().calculateBoundingBox().getWidth()/0.125f),new Quaternion(),new Vector3(0.125f,0.125f,0.125f));
+		middleFloorIn.transform = new Matrix4(new Vector3(0,getFloorHeight(),getFloorDepth()),new Quaternion(),new Vector3(0.125f,0.125f,0.125f));
 		topFloorIn.transform = new Matrix4(new Vector3(0,0,0),new Quaternion(),new Vector3(0.125f,0.125f,0.125f));
 
 
 		instances.add(firstFloorIn);
 		instances.add(interiorFirstFloorIn);
 		instances.add(middleFloorIn);
-		//instances.add(topFloorIn);
+		instances.add(topFloorIn);*/
+		Array<ModelInstance> floors = new Array<>();
+
+		for (int i = 0; i < 10; i++){
+			ModelInstance aux = null;
+			ModelInstance aux2 = null;
+			if(i==0){
+				aux = new ModelInstance(firstFloor);
+				aux.transform = new Matrix4(new Vector3(0,0,0),new Quaternion(),new Vector3(SCALE,SCALE,SCALE));
+
+				aux2 = new ModelInstance(interiorfirstFloor);
+				aux2.transform = new Matrix4(new Vector3(0,0,0),new Quaternion(),new Vector3(SCALE,SCALE,SCALE));
+
+				floors.add(aux);
+				floors.add(aux2);
+			}else if(i<9){
+				aux = new ModelInstance(middleFloor);
+				aux.transform = new Matrix4(new Vector3(0,getFloorHeight()*i,0),new Quaternion(),new Vector3(SCALE,SCALE,SCALE));
+
+				aux2 = new ModelInstance(interiorMiddleFloor);
+				aux2.transform = new Matrix4(new Vector3(0,getFloorHeight()*i,0),new Quaternion(),new Vector3(SCALE,SCALE,SCALE));
+				floors.add(aux);
+				floors.add(aux2);
+			}else if(i<10){
+				aux = new ModelInstance(topFloor);
+				aux.transform = new Matrix4(new Vector3(0,getFloorHeight()*i,0),new Quaternion(),new Vector3(SCALE,SCALE,SCALE));
+
+				aux2 = new ModelInstance(interiorTopFloor);
+				aux2.transform = new Matrix4(new Vector3(0,getFloorHeight()*i,0),new Quaternion(),new Vector3(SCALE,SCALE,SCALE));
+				floors.add(aux);
+				floors.add(aux2);
+			}
+			instances.add(aux);
+			instances.add(aux2);
+		}
 
 
 
@@ -206,5 +244,21 @@ public class GameScreen implements Screen {
 	public void dispose () {
 		modelBatch.dispose();
 		
+	}
+
+	public float getFloorHeight(){
+		float unscaled_h = middleFloor.meshes.first().calculateBoundingBox().getHeight();
+		float actual_h = unscaled_h/2+unscaled_h/SCALE*1.5f; //el 1.5 y el 2 han salido de ensayo y error, no se sabe q logica tiene
+	return actual_h;
+	}
+	public float getFloorWidth(){
+		float unscaled_w = middleFloor.meshes.first().calculateBoundingBox().getWidth();
+		float actual_w = unscaled_w/2+unscaled_w/SCALE*1.5f;
+		return actual_w;
+	}
+	public float getFloorDepth(){
+		float unscaled_d = middleFloor.meshes.first().calculateBoundingBox().getDepth();
+		float actual_d = unscaled_d/2+unscaled_d/SCALE*1.5f;
+		return actual_d;
 	}
 }
